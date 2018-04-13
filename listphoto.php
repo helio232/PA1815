e13d734f20079d0dfced48fef1d67bdc50e43226
Carmeny1023 <38229688+Carmeny1023@users.noreply.github.com>
2018-04-13 19:20:13 +1000

    listphoto

--- listphoto.php
+++ listphoto.php
@@ -1,16 +1,35 @@
+
 <?php
 
-//list of photos
-	$photos = glob("test photo/*.jpg");
+header('Content-Type: application/json');
 
-	usort($photos, function($a, $b) {
-	    return filemtime($a) < filemtime($b);
-	});
+$photos = glob("test photo/*.jpg");
 
-	for ($i=0; $i < count($photos); $i++){
-		$photo = $photos[$i];
-		echo $photo;
-	}
+// 	usort($photos, function($a, $b) {
+// 	    return filemtime($a) < filemtime($b);
+// 	});
 
+$list = array(); //main array
 
+ 	for ($i=0; $i < count($photos); $i++){
+ 		$photo = $photos[$i];
+
+ 		$photolist = array(
+                'filename' => basename($photo), 
+                'size' => filesize($photo) );
+                array_push($list, $photolist);
+
+ 	}
+    $return_array = array('files'=> $list);
+
+    //echo json_encode($return_array);
+    $data = json_encode($return_array);
+    
+    //output a JSON FILE
+    //file_put_contents('filelist.json', $data);
+    
+    echo $data;
+    
 ?>
+
+
